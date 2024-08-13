8a5cd9f90daae8e52acf6673c62b61090940f9b8
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-08-13 13:23:18 -0400

    Better display
    Also remove origin trace for less networking

--- gmsv_bullettracer/lua/autorun/gmsv_bullettracer.lua
+++ gmsv_bullettracer/lua/autorun/gmsv_bullettracer.lua
@@ -10,62 +10,44 @@ end
 
 gmsv.StartModule("BulletTracer")
 do
-	if SERVER then
-		function EntityFireBullets(Entity, Data)
-			local HullSize = Data.HullSize or 0
-			local Hull = Vector(HullSize, HullSize, HullSize)
+	if CLIENT then
+		local BoxMins = Vector(-2, -2, -2)
+		local BoxMaxs = Vector(2, 2, 2)
 
-			local Trace = util.TraceHull({
-				start = Data.Src,
-				endpos = Data.Src + (Data.Dir * Data.Distance),
-				mins = -Hull,
-				maxs = Hull,
-				filter = Entity
-			})
+		function DisplayTracer(self, StartPos, EndPos, Color)
+			debugoverlay.Line(StartPos, EndPos, 3, Color)
 
-			net.Start("gmsv_BulletTracer")
-				net.WriteBool(true)
-				net.WriteVector(Trace.StartPos)
-				net.WriteVector(Trace.HitPos)
-			net.Broadcast()
+			if Color ~= color_white then
+				debugoverlay.Box(EndPos, BoxMins, BoxMaxs, 3, Color)
+			end
 		end
-	elseif CLIENT then
+
 		net.Receive("gmsv_BulletTracer", function()
 			if not self:GetEnabled() then return end
 
-			local IsOrigin = net.ReadBool()
 			local StartPos = net.ReadVector()
 			local EndPos = net.ReadVector()
 
-			debugoverlay.Line(StartPos, EndPos, 3, IsOrigin and color_white or color_blue)
+			self:DisplayTracer(StartPos, EndPos, color_blue)
 		end)
 	end
 
 	function PostEntityFireBullets(Entity, Data)
 		if SERVER then
 			net.Start("gmsv_BulletTracer")
-				net.WriteBool(false)
 				net.WriteVector(Data.Trace.StartPos)
 				net.WriteVector(Data.Trace.HitPos)
 			net.Broadcast()
 		elseif CLIENT then
-			debugoverlay.Line(Data.Trace.StartPos, Data.Trace.HitPos, 3, color_red)
+			self:DisplayTracer(Data.Trace.StartPos, Data.Trace.HitPos, color_red)
 		end
 	end
 
 	function OnEnabled(self)
-		if SERVER then
-			hook.Add("EntityFireBullets", self:GetName(), self.EntityFireBullets)
-		end
-
 		hook.Add("PostEntityFireBullets", self:GetName(), self.PostEntityFireBullets)
 	end
 
 	function OnDisabled(self)
-		if SERVER then
-			hook.Remove("EntityFireBullets", self:GetName())
-		end
-
 		hook.Remove("PostEntityFireBullets", self:GetName())
 	end
 end
